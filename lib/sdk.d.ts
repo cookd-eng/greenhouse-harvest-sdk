d70e8a75cae196875a8200141949c1208d532d76
aalobai1 <46427213+aalobai1@users.noreply.github.com>
2024-08-15 11:39:52 -0400

    add jobs and jobPosts to GreenhouseHarvestSDK

--- lib/sdk.d.ts
+++ lib/sdk.d.ts
@@ -1,11 +1,15 @@
 import { HarvestApplicationsApi } from "./gh_applications";
 import { HarvestCustomFieldsApi } from "./gh_custom_fields";
 import { HarvestCandidatesApi } from "./gh_candidates";
+import { HarvestJobPostsApi } from "./gh_job_posts";
+import { HarvestJobsApi } from "./gh_jobs";
 declare class GreenhouseHarvestSDK {
     private client;
     applications: HarvestApplicationsApi;
     customFields: HarvestCustomFieldsApi;
     candidates: HarvestCandidatesApi;
+    jobs: HarvestJobsApi;
+    jobPosts: HarvestJobPostsApi;
     constructor(apiKey: string, baseURL?: string);
 }
 export default GreenhouseHarvestSDK;

87d1a7914d5ef1270a1f53df7b4fa0e331da722f
aalobai1 <46427213+aalobai1@users.noreply.github.com>
2024-08-15 12:36:50 -0400

    Remove logging

--- src/base_api.ts
+++ src/base_api.ts
@@ -10,7 +10,6 @@ export class BaseGreenhouseHarvestSDK {
   protected async request<T>(config: AxiosRequestConfig): Promise<T> {
     try {
       const response = await this.client(config);
-      console.log(`Response: ${JSON.stringify(response.data, null, 2)}`);
       return response.data;
     } catch (error) {
       if (axios.isAxiosError(error)) {
